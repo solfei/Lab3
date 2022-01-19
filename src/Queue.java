10507ba249d85b8405c23254cc35070c3644b7d3
Sam Yadav <samyadave@gmail.com>
2022-01-18 21:34:19 -0800

    Lab 3 work (1/18)

--- src/Queue.java
+++ src/Queue.java
@@ -27,7 +27,9 @@ public class Queue<T> implements Q<T> {
      * Default constructor for the Queue class
      */
     public Queue() {
-
+        this.front = null;
+        this.end = null;
+        this.size = 0;
     }
 
     /**
@@ -37,7 +39,13 @@ public class Queue<T> implements Q<T> {
      *              the Queue
      */
     public Queue(T[] array) {
-
+        this();
+        if (array == null) {
+            return;
+        }
+        for (int i = 0; i < array.length; i++) {
+            this.enqueue(array[i]);
+        }
     }
 
     /**
@@ -47,16 +55,86 @@ public class Queue<T> implements Q<T> {
      * @param aQueue another Queue to copy
      */
     public Queue(Queue<T> aQueue) {
+        if (aQueue == null) {
+            return;
+        }
 
+        if (aQueue.size == 0) {
+            this.front = null;
+            this.end = null;
+            this.size = 0;
+        } else {
+            Node temp = aQueue.front;
+            while (temp != null) {
+                enqueue(temp.data);
+                temp = temp.next;
+            }
+        }
     }
 
     /**** ACCESSORS ****/
 
     // add methods here
+    public T getFront() throws NoSuchElementException {
+        if (this.size == 0) {
+            throw new NoSuchElementException("getFront(): Queue is empty.");
+        }
+
+        return this.front.data;
+    }
+
+    /**
+     * Returns the size of the Queue
+     * 
+     * @return the size from 0 to n
+     */
+    public int getSize() {
+        return this.size;
+    }
+
+    /**
+     * Determines whether a Queue is empty
+     * 
+     * @return whether the Queue contains
+     *         no elements
+     */
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
 
     /**** MUTATORS ****/
 
     // add methods here
+
+    public void enqueue(T data) {
+        Node N = new Node(data);
+        if (this.size == 0) {
+            this.front = this.end = N;
+        } else {
+            this.end.next = N;
+        }
+        size++;
+    }
+
+    public void dequeue() throws NoSuchElementException {
+        if (this.isEmpty()) {
+            throw new NoSuchElementException("dequeue(): List is empty!");
+        }
+
+        if (this.size == 1) {
+            this.front = this.end = null;
+        } else {
+            this.front = this.front.next;
+        }
+    }
+
+    public String reverseQueue() {
+        String result = "";
+
+        reverseQueue(this.end);
+
+        return result;
+    }
 
     /**** ADDITONAL OPERATIONS ****/
 
@@ -69,7 +147,15 @@ public class Queue<T> implements Q<T> {
      */
     @Override
     public String toString() {
-        return null;
+        StringBuilder result = new StringBuilder("");
+
+        Node temp = this.front;
+        while (temp != null) {
+            result.append(temp.data);
+            temp = temp.next;
+        }
+
+        return result.toString() + " \n";
     }
 
     /**
@@ -81,7 +167,26 @@ public class Queue<T> implements Q<T> {
      */
     @Override
     public boolean equals(Object o) {
-        return false;
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Queue)) {
+            return false;
+        }
+        Queue<T> obj = (Queue<T>) o;
+        if (this.size != obj.size) {
+            return false;
+        }
+        Node thisTemp = this.front;
+        Node objTemp = obj.front;
+        while (thisTemp != null) {
+            if (!(thisTemp.data.equals(objTemp.data))) {
+                return false;
+            }
+            thisTemp = thisTemp.next;
+            objTemp = objTemp.next;
+        }
+        return true;
     }
 
     /** RECURSIVE HELPER METHODS */
@@ -92,7 +197,36 @@ public class Queue<T> implements Q<T> {
      * 
      * @param n the current node
      */
+
     private String reverseQueue(Node n) {
-        return null;
+        Node temp = this.front;
+
+        if (temp.next != n) {
+            temp = temp.next;
+            reverseQueue();
+        }
+
+        return temp.data.toString();
+    }
+
+    public static void main(String[] args) {
+        Queue<String> q1 = new Queue<>();
+        for (int i = 'A'; i < 'M'; i++) {
+            q1.enqueue("" + (char) i);
+        }
+        Queue<Integer> q2 = new Queue<>();
+        Queue<Double> q3 = new Queue<>(new Double[] { 1.1, 2.2, 3.3, 4.4 });
+
+        System.out.println(q2.isEmpty());
+        try {
+            q2.getFront();
+        } catch (Exception e) {
+            System.out.println("YAAAY");
+        }
+
+        int size = q3.getSize();
+        System.out.println("Is getSize() working?... " + (size == 4));
+        System.out.println("");
+
     }
 }
